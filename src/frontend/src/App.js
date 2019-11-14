6bcf392c80aefd95425d2792e8f66b14a57a212a
BenKats <katsbenjamin@gmail.com>
2019-11-13 20:03:16 -0500

    Linted commented out code

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
 
-import Ingredient from "./Ingredient.js";
 import AutocompleteContainer from "./components/AutocompleteContainer";
 class App extends Component {
   state = {
@@ -16,46 +15,12 @@ class App extends Component {
     value: ""
   };
 
-  // newIngredientInputHandler = event => {
-  //   this.setState({
-  //     value: event.target.value
-  //   });
-  // };
-
-  // addIngredientHandler = event => {
-  //   event.preventDefault();
-  //   // console.log(this.state.value);
-  //   const ingred = [...this.state.ingredients];
-  //   ingred.push({ name: this.state.value });
-  //   this.setState({ ingredients: ingred });
-  // };
-
-  // handleInputChange = event => {
-  //   this.setState({ value: event.target.value });
-  // };
-
   render() {
     return (
       <div className="App">
         <h1>Hello, Let's Eat!</h1>
         <p>Select Your Ingredients</p>
-        {/* <Ingredient
-          name={this.state.ingredient.name}
-          changed={this.newIngredientInputHandler}
-        />
-        {this.state.ingredients.map((ingredient, index) => {
-          return <Ingredient name={ingredient.name} key={index} />;
-        })}
-        <form onSubmit={event => this.addIngredientHandler(event)}>
-          <label>
-            Ingredient:
-            <input
-              type="text"
-              onChange={event => this.handleInputChange(event)}
-            />
-          </label>
-          <input type="submit" />
-        </form> */}
+
         <AutocompleteContainer />
       </div>
     );
