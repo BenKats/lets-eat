0e3959db9dd3ed8f3a16ea45cff0e19f5d0dd85f
BenKats <katsbenjamin@gmail.com>
2019-11-13 20:15:26 -0500

    Added border for cards

--- src/frontend/src/components/Card.js
+++ src/frontend/src/components/Card.js
@@ -3,7 +3,9 @@ import React from "react";
 const Card = props => {
   const cardStyle = {
     display: "inline-block",
-    margin: "0px 5px"
+    margin: "0px 5px",
+    padding: "0px 5px",
+    border: "2px solid black"
   };
   return (
     <p style={cardStyle} onClick={props.deleteCardHandler}>
