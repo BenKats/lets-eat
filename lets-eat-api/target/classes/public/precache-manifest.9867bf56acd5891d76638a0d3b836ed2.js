5e882b05b709c4a399df44548a5484c2603ed909
BenKats <katsbenjamin@gmail.com>
2019-11-15 10:24:02 -0500

    deploying to aws

---
+++ lets-eat-api/target/classes/public/precache-manifest.9867bf56acd5891d76638a0d3b836ed2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e698bc5390195c6efd06b25ed3ba780d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b788022ce721c9a0a82",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "adca3d13c020463d205c",
+    "url": "/static/js/2.67fddd3f.chunk.js"
+  },
+  {
+    "revision": "9b788022ce721c9a0a82",
+    "url": "/static/js/main.d444dfde.chunk.js"
+  },
+  {
+    "revision": "9aabb23bcbd2c9e69d36",
+    "url": "/static/js/runtime-main.baeb1881.js"
+  }
+]);
