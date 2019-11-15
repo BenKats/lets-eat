a8ba515615af11b66936939bf8c02099253874bc
BenKats <katsbenjamin@gmail.com>
2019-11-15 00:52:32 -0500

    User can sign up and log in

--- src/frontend/src/components/UserEntry/EntryContainer.js
+++ src/frontend/src/components/UserEntry/EntryContainer.js
@@ -18,10 +18,53 @@ class EntryContainer extends Component {
 
   signupHandler = () => {
     console.log("signupHandler Called");
+    this.fetchSignup();
   };
 
   loginHandler = () => {
     console.log("loginHandler Called");
+    this.fetchLogin();
+  };
+
+  fetchSignup = () => {
+    fetch(`http://localhost:8181/signup`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        username: this.state.email,
+        password: this.state.password
+      })
+    })
+      .then(res => {
+        // console.log(res);
+        return res.json();
+      })
+      .then(res => {
+        console.log(res);
+        window.sessionStorage.setItem("token", res.token);
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  };
+
+  fetchLogin = () => {
+    fetch(`http://localhost:8181/login`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        username: this.state.email,
+        password: this.state.password
+      })
+    })
+      .then(res => {
+        // console.log(res);
+        return res.json();
+      })
+      .then(res => {
+        console.log(res);
+        window.sessionStorage.setItem("token", res.token);
+      });
   };
   render() {
     return (
